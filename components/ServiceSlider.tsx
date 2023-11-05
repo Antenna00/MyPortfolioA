523a216f5a0c27e0e2486c305e66ececb7bb18f7
RinKit6024 <butasan6024@gmail.com>
2023-11-06 01:20:32 +0900

    ttt

--- components/ServiceSlider.tsx
+++ components/ServiceSlider.tsx
@@ -72,14 +72,14 @@ function ServiceSlider() {
           serviceData.map((item, index) => {
             return (
             <SwiperSlide key={index}>
-              <div className="bg-[rgba(65,47,123,0.15)] h-[300px] rounded-lg px-6 py-8 flex xl:flex-col 
+              <div className="bg-[rgba(65,47,123,0.15)] h-[500px] rounded-lg px-6 py-8 flex xl:flex-col 
               gap-x-6 xl:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
                 {/* Icon */}
                 <div className="text-4xl text-accent mb-4">{item.icon}</div>
                 {/* title & description */}
                 <div className="mb-8">
                   <div>{item.title}</div>
-                  <p>{item.description}</p>
+                  <p>{item.description}</p> 
                 </div>
                 {/* arrow */}
                 <div className='text-3xl'>
