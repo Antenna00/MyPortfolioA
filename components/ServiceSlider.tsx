c115647ea2002234cb9550384c5936f218dc632e
RinKit6024 <butasan6024@gmail.com>
2023-11-06 01:26:35 +0900

    test

--- components/ServiceSlider.tsx
+++ components/ServiceSlider.tsx
@@ -62,6 +62,7 @@ export const serviceData:serviceInfo[] = [
 function ServiceSlider() {
   return (
     <Swiper 
+      spaceBetween={10}
       pagination={{
         clickable: true
       }}
@@ -72,8 +73,7 @@ function ServiceSlider() {
           serviceData.map((item, index) => {
             return (
             <SwiperSlide key={index}>
-              <div className="bg-[rgba(65,47,123,0.15)] h-[500px] rounded-lg px-6 py-8 flex xl:flex-col 
-              gap-x-6 xl:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+              <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
                 {/* Icon */}
                 <div className="text-4xl text-accent mb-4">{item.icon}</div>
                 {/* title & description */}
